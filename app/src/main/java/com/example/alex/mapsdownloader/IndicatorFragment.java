e71c3c7289995cd3d5502a9cab0cce6020817373
alex <asytnyk0@gmail.com>
2017-05-07 13:58:34 +0300

    Create IndicatorFragment and related stuff

---
+++ app/src/main/java/com/example/alex/mapsdownloader/IndicatorFragment.java
@@ -0,0 +1,19 @@
+package com.example.alex.mapsdownloader;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IndicatorFragment extends Fragment {
+
+    public IndicatorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_indicator, container, false);
+    }
+}
